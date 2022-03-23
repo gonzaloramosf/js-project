0e75ee5e0f486ff6923689c4fc8619e4adb41c5b
gonzaloramosf <gonza.farinho@hotmail.com>
2022-03-23 19:19:29 -0300

    App.js - Styles

--- app.js
+++ app.js
@@ -4,18 +4,22 @@ const Database = () => {
             id: 1,
             title: 'Peugeot 308',
             price: 1800000, 
+            detailPage: "itemPage.html",
         },
         {
             id: 2,
             title: 'Peugeot 208',
-            price: 2200000, 
+            price: 2200000,
+            detailPage: "itemPage.html", 
         },
         {
             id: 3,
             title: 'Peugeot 307',
             price: 1000000, 
+            detailPage: "itemPage.html",
         }
     ];
+
     ShowItems(items);
 }
 
@@ -26,22 +30,25 @@ const ShowItems = (items) => {
     items.forEach((item) => {
         // item features container
         let itemContainer = document.createElement("div");
-        
+        itemContainer.className = "card item";
+
         // adding item.title
         let itemTitle = document.createElement("h4");
-        itemTitle.textContent= `${item.title}`
+        itemTitle.textContent= `${item.title}`;
+        
         // adding item.img
 
         // adding ite.price
-        let itemPrice = document.createElement("span")
-        itemPrice.textContent = `${item.price}`
+        let itemPrice = document.createElement("span");
+        itemPrice.textContent = `${item.price}`;
 
-        // button addToCart
+        // button product page
         let itemButton = document.createElement("button");
-        itemButton.textContent = "Add to cart"
+        itemButton.textContent = "View details";
+        // navigate to product page
         itemButton.addEventListener("click", () => {
-            let itemId = item.id;
-            AddToCart(itemId);
+            Item(item);
+            console.log('ocu pado');
         });
 
         // adding features
@@ -53,105 +60,47 @@ const ShowItems = (items) => {
         // itemContainer whit all features
         itemsContainer.appendChild(itemContainer);
     })
-};
+}
+
+const Item = (item) => {
+
+    // calling section for item in html 
+    const itemDetailContainer = document.querySelector(".item");
+
+    // item detail container
+    let itemContainer = document.createElement("div");
+
+    // item.title 
+    let itemTitle = document.createElement("h4");
+    itemTitle.textContent= `${item.title}`;
+    // item.img
+    // item.price
+    let itemPrice = document.createElement("span");
+    itemPrice.textContent = `${item.price}`;
+
+    // adding features
+    itemContainer.appendChild(itemTitle);
+    // item.img
+    itemContainer.appendChild(itemPrice);
+
+    itemDetailContainer.appendChild(itemContainer);
+    // itemContainer whit all features
+    
+    // item count
+
+    // add to cart
+}
 
 const AddToCart = (itemId) => {
     console.log(itemId);
 }
-
 
 // initialization
 Database();
 
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// const elegirUnAuto = () => {
-//     let autoId;
-//     do{
-//         autoId = parseInt(prompt('Ingresa el la opcion que deseas compra: 1 para 308, 2 para 208, 3 para 307'));
-//     }while(isNaN(autoId))
-    
-//     infoAutoSeleccionado(autoId);
-// }
-
-// const infoAutoSeleccionado = (autoId) => {
-//     const items = [
-//         {
-//             id: 1,
-//             title: 'Peugeot 308',
-//             price: 1800000, 
-//         },
-//         {
-//             id: 2,
-//             title: 'Peugeot 208',
-//             price: 2200000, 
-//         },
-//         {
-//             id: 3,
-//             title: 'Peugeot 307',
-//             price: 1000000, 
-//         }
-//     ];
-    
-//     const autoSeleccionado = items.find((element) =>  element.id === autoId);
-//     ingresoDePrecioYcuotas(autoSeleccionado);
-// }
-
-// const ingresoDePrecioYcuotas = (autoSeleccionado) => {
-//     let precio = autoSeleccionado.price;
-//     let cuotas;
-
-//     alert('El precio del auto ' + autoSeleccionado.title + ' es de ' + autoSeleccionado.price);
-    
-//     do{
-//         cuotas = parseInt(prompt('Ingresa la cantidad de cuotas en que deseas pagar el producto'));
-//     }while(isNaN(cuotas))
-//     costoPorCuota(precio, cuotas);
-// }
-
-// const costoPorCuota = (precio, cuotas) => {
-//     let precioAPagarMensual = precio / cuotas;
-//     alert(`El precio a pagar mensualmente por el producto es de ${precioAPagarMensual}`);
-// }   
-
-// elegirUnAuto();
+// itemButton.addEventListener("click", () => {
+//     let itemId = item.id;
+//     AddToCart(itemId);
+// });
