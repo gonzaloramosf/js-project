bb92da67f447640e5d49f98ab0fc7729def3924c
gonzaloramosf <gonza.farinho@hotmail.com>
2022-04-05 21:00:12 -0300

    bt-modal fixed - toastify

--- app.js
+++ app.js
@@ -48,7 +48,7 @@ const ShowItems = (items) => {
         let itemPrice = document.createElement("span");
         itemPrice.textContent = `${item.price}`;
 
-        // adding item btn
+        // adding item detail btn
         let detailsButton = document.createElement("button");
         detailsButton.textContent = "View details";
         detailsButton.className = "btn btn-primary";
@@ -75,7 +75,6 @@ const ShowItems = (items) => {
         itemContainer.appendChild(itemPrice);
         itemContainer.appendChild(detailsButton);
         itemContainer.appendChild(toCartButton);
-
         // itemContainer whit all features
         itemsContainer.appendChild(itemContainer);
     })
@@ -83,53 +82,55 @@ const ShowItems = (items) => {
 
 const ActivateModal = (item) => {
     let modalWrap = null;
-    const showModal = () => {
-        if (modalWrap !== null) {
-            modalWrap.remove();
-        }
-        modalWrap = document.createElement("div");
+    if (modalWrap !== null) {
+        modalWrap.remove();
+    }
+    modalWrap = document.createElement("div");
 
-        let modalBox = document.createElement("div");
-        modalBox.className = "modal fade";
+    let modalBox = document.createElement("div");
+    modalBox.className = "modal fade";
         let modalDialog = document.createElement("div");
-        modalDialog.className = "modal-dialog";
-        let modalContent = document.createElement("div");
-        modalContent.className = "modal-content";
-        let modalHeader = document.createElement("div");
-        modalHeader.className = "modal-header";
-        let modalBody = document.createElement("div");
-        modalBody.className = "modal-body";
-        let modalFooter = document.createElement("div");
-        modalBody.className = "modal-footer";
-        // let modalCloseBtn = document.createElement("button");
-        // modalCloseBtn.className = "close hide";
+        modalDialog.className = "modal-dialog modal-dialog-centered";
+            let modalContent = document.createElement("div");
+            modalContent.className = "modal-content";
+                let modalHeader = document.createElement("div");
+                modalHeader.className = "modal-header";
+                let modalBody = document.createElement("div");
+                modalBody.className = "modal-body";
+                let modalFooter = document.createElement("div");
+                modalBody.className = "modal-footer";
+            let modalCloseBtn = document.createElement("button");
+            modalCloseBtn.className = "close";
+            modalCloseBtn.textContent = "x";
+            modalCloseBtn.addEventListener("click", () => {modal.hide()});
+    // --  
 
-        let itemTitle = document.createElement("h4");
-        itemTitle.textContent = `${item.title}`;
-        itemTitle.className = "modal-title";
-        let itemPrice = document.createElement("span");
-        itemPrice.textContent = `${item.price}`;
-        let itemDetail = document.createElement("p");
-        itemDetail.textContent = `${item.detail}`;
+    let itemTitle = document.createElement("h3");
+    itemTitle.textContent = `${item.title}`;
+    itemTitle.className = "modal-title";
+    let itemPrice = document.createElement("span");
+    itemPrice.textContent = `${item.price}`;
+    let itemDetail = document.createElement("p");
+    itemDetail.textContent = `${item.detail}`;
 
+    // modalBox content
+    modalBox.appendChild(modalDialog);
+    modalHeader.appendChild(itemTitle);
+    modalHeader.appendChild(modalCloseBtn);
+    modalBody.appendChild(itemDetail);
+    modalFooter.appendChild(itemPrice);
+    // adding modal content
+    modalContent.appendChild(modalHeader);
+    modalContent.appendChild(modalBody);
+    modalContent.appendChild(modalFooter);
+    // modal content
+    modalDialog.appendChild(modalContent);
 
-        // adding features
-        modalBox.appendChild(modalDialog);
-        modalHeader.appendChild(itemTitle);
-        // modalHeader.appendChild(modalCloseBtn);
-        modalBody.appendChild(itemDetail);
-        modalFooter.appendChild(itemPrice);
-        modalContent.appendChild(modalHeader);
-        modalContent.appendChild(modalBody);
-        modalContent.appendChild(modalFooter);
-        modalDialog.appendChild(modalContent);
-        modalWrap.appendChild(modalBox);
-        document.body.append(modalWrap);
-        
-        let modal = new bootstrap.Modal(modalWrap.querySelector('.modal'));
-        modal.show();
-    } 
-    showModal();
+    // adding modalBox whit all content - styles
+    modalWrap.appendChild(modalBox);
+    document.body.append(modalWrap);
+    let modal = new bootstrap.Modal(modalWrap.querySelector('.modal'));
+    modal.show();
 }
 
 const AddToCart = (item) => {
